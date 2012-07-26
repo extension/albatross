d30d0478534ddd4da6cf292436a46f705dd4fd71
sdnall <s.daniel.nall@gmail.com>
2012-07-26 19:46:57 -0400

    use text fields to store stdout and stderrr

--- db/migrate/20120723151153_create_cron_logs.rb
+++ db/migrate/20120723151153_create_cron_logs.rb
@@ -2,8 +2,8 @@ class CreateCronLogs < ActiveRecord::Migration
   def change
     create_table :cron_logs do |t|
       t.references :cron
-      t.string      "stdout"
-      t.string      "stderr"
+      t.text        "stdout", :limit => 16777215
+      t.text        "stderr", :limit => 16777215
       t.string      "command"
       t.string      "server"
       t.datetime    "started_at"
