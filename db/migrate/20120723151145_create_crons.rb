4a7277b4dcef57c5d08a8833477a18329aa10d4f
sdnall <s.daniel.nall@gmail.com>
2012-07-26 17:35:02 -0400

    add some needed defaults

--- db/migrate/20120723151145_create_crons.rb
+++ db/migrate/20120723151145_create_crons.rb
@@ -1,8 +1,8 @@
 class CreateCrons < ActiveRecord::Migration
   def change
     create_table :crons do |t|
-      t.string      "name"
-      t.boolean     "notify_on_error"
+      t.string      "name", :null => false
+      t.boolean     "notify_on_error", :null => false, :default => true
       t.timestamps
     end
   end
