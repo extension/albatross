9a0e32a50397682c4df5884933f2542238bf43c6
Jason Adam Young <jayoung@extension.org>
2012-09-11 14:04:17 -0400

    use the path that's theoretically being symlinked

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@
 require 'omniauth-openid'
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :open_id,  :store => OpenID::Store::Filesystem.new("#{Rails.root}/tmp"), :name => 'people', :identifier => 'https://people.extension.org'
+  provider :open_id,  :store => OpenID::Store::Filesystem.new("#{Rails.root}/tmp/omniauth"), :name => 'people', :identifier => 'https://people.extension.org'
 end
 
