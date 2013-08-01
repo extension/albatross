4497a00b6311da31a3cb0d3eb1b7f5fc06ea5caa
Jason Adam Young <jayoung@extension.org>
2013-08-01 13:45:57 -0400

    BTSNOCAP

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,3 +1,8 @@
 set :rails_env, 'production'
 set :deploy_to, '/services/engineering/'
+if(branch = ENV['BRANCH'])
+  set :branch, branch
+else
+  set :branch, 'development'
+end
 server 'dev-engineering.extension.org', :app, :web, :db, :primary => true
