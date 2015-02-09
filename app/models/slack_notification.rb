17f083264ae0dafb87f345b2930860ec0782cfee
Jason Adam Young <jayoung@extension.org>
2015-02-09 13:09:27 -0500

    Allow for custom emoji icons in slack posts

--- app/models/slack_notification.rb
+++ app/models/slack_notification.rb
@@ -5,7 +5,7 @@
 
 class SlackNotification
 
-  attr_accessor :message, :attachment, :slack
+  attr_accessor :message, :attachment, :slack, :icon_emoji
 
   def initialize(options = {})
     username = options[:username] || "Engineering Notifier"
@@ -15,15 +15,24 @@ class SlackNotification
     @slack.channel = channel
     @message = options[:message] || ''
     @attachment = options[:attachment]
+    @icon_emoji = options[:icon_emoji]
     self
   end
 
   def post
+    post_parameters = {}
     if(self.attachment)
-      self.slack.ping(self.message, attachments: [self.attachment], icon_url: 'https://engineering.extension.org/favicon.ico')
-    else
-      self.slack.ping(self.message, icon_url: 'https://engineering.extension.org/favicon.ico')
+      post_parameters[:attachments] = [self.attachment]
     end
+
+    if(self.icon_emoji)
+      post_parameters[:icon_emoji] = self.icon_emoji
+    else
+      post_parameters[:icon_emoji] = 'https://engineering.extension.org/favicon.ico'
+    end
+
+    self.slack.ping(self.message, post_parameters)
+
   end
 
   def self.post(options = {})
