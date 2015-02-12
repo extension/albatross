e10d4c20addf77c37883563c03f637138055df47
Jason Adam Young <jayoung@extension.org>
2015-02-12 14:56:11 -0500

    Add slack error notification for cron errors

--- app/models/cronmon_log.rb
+++ app/models/cronmon_log.rb
@@ -4,6 +4,8 @@
 # see LICENSE file
 
 class CronmonLog < ActiveRecord::Base
+  include Rails.application.routes.url_helpers
+  default_url_options[:host] = Settings.urlwriter_host
 
   belongs_to :cronmon
   has_one :cronmon_log_output, :dependent => :destroy
@@ -29,7 +31,42 @@ class CronmonLog < ActiveRecord::Base
   def notify_if_error(force = false)
     if(force or !self.success?)
       Notification.create(notifiable: self, notification_type: Notification::CRONMON_ERROR)
+      self.error_notification
     end
+  end
+
+  def log_url
+    showlog_cronmon_url(self.cronmon,log_id: self.id)
+  end
+
+  def error_notification
+      post_options = {}
+      post_options[:channel] = Settings.systems_slack_channel
+      post_options[:username] = "Cronmon Notification"
+      post_options[:icon_emoji] = ':clock5:'
+
+      attachment = { "fallback" => "rotating_light:  #{self.cronmon.label} execution error! Details #{self.log_url} rotating_light:",
+      "text" => ":rotating_light: #{self.cronmon.label} execution error! :rotating_light:",
+      "fields" => [
+        {
+          "title" => "Server",
+          "value" => "#{self.cronmon.cronmon_server.name}",
+          "short" => false
+        },
+        {
+          "title" => "Command",
+          "value" =>  "#{self.command}",
+          "short" =>  false
+        }
+      ],
+      "color" => "danger"
+    }
+
+    attachment["fields"].push({"title" => "Details", "value" => self.log_url, "short" => false})
+    post_options[:attachment] = attachment
+
+    SlackNotification.post(post_options)
+
   end
 
 end
