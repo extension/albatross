b43b9e22900e1d6b92004d902bc0e0b008cf06d7
Jason Adam Young <jayoung@extension.org>
2015-03-09 12:37:56 -0400

    hardcode reference

--- app/models/slack_notification.rb
+++ app/models/slack_notification.rb
@@ -4,8 +4,6 @@
 # see LICENSE file
 
 class SlackNotification
-  include Rails.application.routes.url_helpers
-  default_url_options[:host] = Settings.urlwriter_host
 
   attr_accessor :message, :attachment, :slack, :icon_emoji
 
@@ -30,7 +28,7 @@ class SlackNotification
     if(self.icon_emoji)
       post_parameters[:icon_emoji] = self.icon_emoji
     else
-      post_parameters[:icon_emoji] = image_url('askbot.png')
+      post_parameters[:icon_emoji] = 'https://engineering.extension.org/assets/askbot.png'
     end
 
     self.slack.ping(self.message, post_parameters)
