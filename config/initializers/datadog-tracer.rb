af66cae432214623fc891303800f9a7ab9ec71af
Jason Adam Young <jayoung@extension.org>
2017-03-06 08:37:44 -0500

    change service name for hopeful host matching

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -2,5 +2,5 @@ Rails.configuration.datadog_trace = {
   enabled: (!Settings.app_location.nil? and Settings.app_location == 'production'),
   auto_instrument: (!Settings.app_location.nil?  and Settings.app_location == 'production'),
   auto_instrument_redis: (!Settings.sidekiq_enabled.nil? and Settings.sidekiq_enabled),
-  default_service: 'engineering'
+  default_service: 'engineering.awsi.extension.org'
 }
