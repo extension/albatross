e78ca88d89b3f4ca3c612aa8bfce5205285e7516
Jason Adam Young <jayoung@extension.org>
2017-03-10 09:33:50 -0500

    Remove DataDog APM due to incredibly confusing billing structure

--- config/initializers/datadog-tracer.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.datadog_trace = {
-  enabled: (!Settings.app_location.nil? and Settings.app_location == 'production'),
-  auto_instrument: (!Settings.app_location.nil?  and Settings.app_location == 'production'),
-  auto_instrument_redis: (!Settings.sidekiq_enabled.nil? and Settings.sidekiq_enabled),
-  default_service: Settings.datadog_service_name
-}
