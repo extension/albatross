d25540b21564dcdc11b2ee93dc6c4e1e94519f88
Jason Young <jay@outfielding.net>
2017-10-13 15:32:35 -0400

    deployment changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :repository,  "git@github.com:extension/albatross.git"
 set :branch, "master"
 set :scm, "git"
 set :user, "pacecar"
+set :gateway, 'deploy.extension.org'
 set :keep_releases, 3
 ssh_options[:forward_agent] = true
 set :port, 22
--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,4 +1,5 @@
 set :rails_env, 'production'
 set :deploy_to, '/services/engineering/'
-set :vhost, 'engineering.awsi.extension.org'
-server vhost, :app, :web, :db, :primary => true
+set :vhost, 'engineering.extension.org'
+set :deploy_server, 'engineering.awsi.extension.org'
+server deploy_server, :app, :web, :db, :primary => true
